e33493b8b86695436a1ae94f1bd068854a9b4fab
tuananhvu319 <69627088+tuananhvu319@users.noreply.github.com>
2020-11-12 10:14:42 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const app = express();
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "Shop API",
+            description: "Backend Api",
+            contact: {
+                name: 'Amazing Developer'
+            },
+            servers: "http://localhost:3636"
+        }
+    },
+    apis: ["app.js", ".routes/*.js"]
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/* CORS */
+app.use(cors({
+    origin: "*",
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Import Routes
+const productsRoute = require('./routes/products');
+const ordersRoute = require('./routes/orders');
+const usersRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
+// Use Routes
+app.use('/api/products', productsRoute);
+app.use('/api/orders', ordersRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/auth', authRoute);
+
+
+
+module.exports = app;
